596f7d25b3c268dda8be6de3d17042d95fd4b41b
fabran99 <46858770+fabran99@users.noreply.github.com>
2019-07-11 15:26:54 -0300

    Add files via upload

---
+++ claseNumeros.js
@@ -0,0 +1,189 @@
+////////////////////////////////
+// numeros
+///////////////////////////////
+// los numeros tienen pocas particularidades en javascript, simplemente
+// hay que tener en cuenta que no pueden estar entre comillas para ser
+// considerados numeros
+
+//////////////////////////
+// 1- operando con numeros
+/////////////////////////
+
+// las operaciones comunes se pueden hacer sin problemas igual que en
+// una calculadora, la diferencia es que se pueden usar variables
+// para representarlos, tambien tener en cuenta que al multiplicar no se usa
+// la x sino *
+
+var numero1 = 5;
+var numero2 = 8;
+
+//suma
+var suma = numero1 + numero2;
+// console.log(suma) => 13
+
+// resta
+var resta = numero1 - numero2;
+// console.log(resta) => -3
+
+// multiplicacion
+var multiplicacion = numero1 * numero2;
+// console.log(multiplicacion) => 40
+
+// division
+var division = numero1 / numero2;
+// console.log(division) => 0.625
+
+// hay otro operador, que lo que hace es indicar el resto que da
+// la división entre 2 numeros, el operador es %
+// por ejemplo al dividir 5 entre 2, da 2.5.
+// En la forma tradicional de dividir, decimos que 5 dividido 2
+// da 2 como resultado y me sobra 1, ese 1 es el resto
+
+var resto = 5 % 2;
+// console.log(resto) => 1
+
+// esto es especialmente util para revisar si un numero es multiplo de otro
+// o si es para o impar, si queremos saber si es par, simplemente usamos este
+// operador para dividirlo entre 2, y revisar si da un resto distinto de 0
+
+var resto2 = 6 % 2;
+// console.log(resto2) => 0
+
+// en este caso el resto es 0 porque 6 se puede dividir entre 2,
+// la division es exacta
+
+/////////////////////////////
+//verificar si numero es par
+/////////////////////////////
+var numeroAverificar = 136;
+
+// if (numeroAverificar % 2 == 0) {
+//   console.log("el numero es par");
+// } else {
+//   console.log("el numero es impar");
+// }
+
+//////////////////////////
+// 2- redondear numeros
+/////////////////////////
+// aveces necesitamos redondear un numero, y hay varias formas de hacerlo
+// redondear al mas cercano, redondear hacia arriba o hacia abajo
+
+// la funcion Math.round(numero) redondea al mas cercano
+// la funcion Math.floor(numero) redondea hacia abajo
+// la funcion Math.ceil(numero) redondea hacia arriba
+
+var numeroConDecimal = 2.3;
+
+// Math.round(numeroConDecimal) => 2
+// Math.floor(numeroConDecimal) => 2
+// Math.ceil(numeroConDecimal) => 3
+
+///////////////////////////
+// 3- redondear hasta ciertas comas
+//////////////////////////
+
+// hay casos donde el numero tiene muchas comas, y pretendemos redondear
+// a cierta cantidad, un ejemplo es al dividir
+
+var tercio = 10 / 3;
+// console.log(tercio) =>3.3333333333333335
+
+// para esto usamos la funcion toFixed(cantidadDeDecimales)
+
+var tercioDecimal = tercio.toFixed(2);
+
+// console.log(tercioDecimal) => 3.33
+
+// es importante notar que toFixed convierte el numero en string, por lo que
+// lo que obtenemos deja de ser un numero, en caso de querer seguir
+// operando con el, lo mejor es convertirlo en numero con parseFloat()
+
+// console.log(tercioDecimal + 1) => 3.331
+
+var tercioDecimalNumerico = parseFloat(tercioDecimal);
+
+// console.log(tercioDecimalNumerico + 1) => 4.331
+
+//////////////////////////
+// 4 - numeros aleatorios
+/////////////////////////
+
+// una forma de conseguir aleatoriedad es usando la funcion Math.random()
+// que genera un numero al azar entre 0 y 1 cada vez que se ejecuta
+
+var azar1 = Math.random();
+// console.log(azar1) =>0.017503556420000344
+
+// esta vez el numero es 0.017503556420000344 pero al volver a ejecutar este
+// archivo, el numero sera distinto
+
+// una limitante de Math.random() es que el numero que devuelve va entre 0 y 1
+// por lo que si queremos un numero entre 0 y 10 por ejemplo,
+//hay que operar con el.
+
+var entre0y10 = Math.random() * 10;
+
+// Lo que se hace es multiplicar el numero aleatorio por el tope
+// al que se quiere llegar, esto hace que si da un numero muy bajo
+// por ejemplo 0.1 el resultado será 1
+// mientras que si da un numero alto como 0.9 el resultado será 9
+
+var entre5y10 = 5 + Math.random() * 5;
+
+// si se quiere establecer el minimo y el maximo lo que se hace es
+// que inicialmente ya este sumado ese maximo antes de mezclarlo con el numero
+// aleatorio, de esta forma te aseguras de que al menos sea 5
+
+// en este caso lo que se hace es a ese 5 inicial, sumarle un numero
+// aleatorio entre 0 y 5, lo que dará como resultado un numero entre 5 y 10
+// para dar un numero exacto se puede usar una funcion de
+//redondeo de las explicadas arriba
+
+//////////////////////////
+// 5- operaciones combinadas
+/////////////////////////////
+// se pueden hacer todo tipo de operaciones combinadas
+//por ejemplo para calcular porcentajes, en algunos casos es necesario
+// el uso de parentesis para separar que trozos de la cuenta son un bloque
+
+var total = 150;
+
+// 150 es el 100%, el 20% es (150 * 20 / 100)
+var veintePorciento = (total * 20) / 100;
+// console.log(veintePorciento) => 30
+
+// si queremos que el porcentaje a calcular sea variable, podemos
+// crear otra variable porcentaje
+
+var porcentaje = 15.5;
+
+var resultado = (total * porcentaje) / 100;
+
+// console.log(resultado) => 23.25
+
+///////////////////////////////
+// ejercicio de logica
+//////////////////////////////
+// los colores rgb estan compuestos por 3 valores, de rojo verde y azul
+// RED, GREEN, BLUE, estos valores van desde 0 hasta 255 y NO PUEDEN
+// LLEVAR COMA, cuanto mas alto mas intenso es ese color en la mezcla final.
+
+// ejemplo 1 :  rgb(256, 0, 0)
+// 256 de rojo, 0 de verde y 0 de azul, da como resultado un rojo intenso
+
+// ejemplo 2 : rgb(0, 0, 0)
+// 0 de todos los colores da como resultado un negro.
+
+// para escribir un color, que luego va a ser usado en una interfaz
+// se debe escribir rgb y entre parentesis los 3 valores de rojo, verde y azul
+// como en los ejemplos, en ese mismo orden.
+
+// usando Math.random() y la mezcla de strings de la parte 9 de claseStrings.js
+// mostrar en la consola una string que forme un color al azar
+// tienen que ser 3 numeros al azar entre 0 y 255
+// ejemplo: rgb(numero1, numero2, numero3)
+
+// en esta pagina se puede poner el codigo rgb de un color y apretando entre
+// te muestra que color forma la mezcla
+// comprobarlo en https://www.w3schools.com/colors/colors_rgb.asp
---
+++ claseStrings.js
@@ -0,0 +1,223 @@
+﻿///////////
+//strings
+///////////
+// una string es una linea de texto, se diferencian por estar rodeadas de comillas
+// pueden ser comillas simples 'string', dobles "string" o especiales `string`
+// la flecha señala el resultado que se mostraria en consola => resultado
+
+///////////////////
+// 1- string basica
+///////////////////
+var str = "test";
+
+// console.log(str) => test
+
+///////////////////////////
+// 2- juntar (se le dice concatenar) varias strings
+///////////////////////////
+// una de las formas de hacerlo, es sumar ambas variables,
+// lo que el programa hace es encadenarlas en una nueva string donde el contenido
+// de ambas variables se pega
+
+var str1 = "string 1";
+var str2 = "string 2";
+
+// console.log(str1 + str2) => string 1string 2
+// en este caso al no tener espacios ambas strings quedan pegadas, se debe dejar
+// un espacio al final de la primera string, o al principio de la segunda
+
+str1 = "string 1 ";
+// console.log(str1 + str2) => string 1 string 2
+
+/////////////////////////
+// 3- mezclar texto con variables string
+////////////////////////
+// al igual que en el ejemplo anterior, simplemente se suma la string a
+// el resto del texto
+
+var str3 = "Jose";
+
+// console.log("Mi nombre es " + str3) => Mi nombre es Jose
+
+///////////////////////////
+//4- Escribir comillas dentro de una string
+///////////////////////////
+// un problema que se puede tener, es querer poner comillas dentro de
+// una string, como una cita de un texto que iria entre comillas
+// en este caso, lo que se suele hacer es, habiendo 3 tipos de comillas "" '' ``
+// simplemente usamos uno para rodear la string, y otro para ser comillas
+// lo que hace javascript, es ignorar las comillas que rodean todo el texto, porque
+// su utilidad es simplemente decirle que lo que esta dentro va a ser una string
+
+var str4 = 'Un sabio dijo una vez: "La vida es dura pero mas dura la verdura"';
+// console.log(str4) => Un sabio dijo una vez: "La vida es dura pero mas dura la verdura"
+
+////////////////////////////
+// 5- Cortar una string
+////////////////////////////
+//Las strings suelen ir de la mano con los arrays, una forma de cortarlas
+//es separarlas en fragmentos, y hacer que esos fragmentos formen un array
+// donde luego se van a poder manejar de forma individual
+
+var str5 = "Manzana,Banana,Anana,Pera,Sandia";
+//Para separar una string en partes, se usa la funcion split()
+// split lleva entre sus parentesis, que caracteres se van a usar para separar
+// la string, es decir, en que lugares la vamos a cortar
+// en el caso de la string str5, las palabras van separadas por coma
+// asique se usaria de la siguiente manera
+
+var stringCortada = str5.split(",");
+
+// console.log(stringCortada) => [ 'Manzana', 'Banana', 'Anana', 'Pera', 'Sandia' ]
+// es importante notar que split, es una funcion que va asociada a todos los strings
+// por defecto, son parte del sistema de javascript, y estas funciones, como otras
+// se las llama de esta forma: string.nombredelafuncion(parametros)
+
+/////////////////////////
+// 6- Convertir string en numero
+/////////////////////////
+// hay casos en que tenemos que operar con un numero, pero el mismo esta entre
+// comillas por lo que es una string, y al usarlo para operar daria errores
+// o resultados inesperados, para esto lo que se usa es la funcion parseFloat()
+// o parseInt()
+
+var str6 = "225";
+// parseInt() sirve para transformar un numero entero, es decir, que no lleve coma
+// parseFloat() sirve para transformar cualquier tipo de numero,
+// inclusive numeros con coma
+
+var transformado = parseInt(str6);
+// en este caso parseInt es una funcion del sistema, y no va asociada necesariamente
+// a las string, por lo que no se pone variable.parseInt()
+// sino que se pone parseInt(variable)
+// con el uso vas a poder identificar cuales funcionan de una manera u otra
+
+str6 = "a25sdd2";
+// si usamos parseInt o parseFloat en una string que ademas del numero, tiene texto
+// pueden pasar 2 cosas: si la string empieza por un caracter que no sea numerico
+// va a dar como resultado NaN, es decir Not A Number, lo que es similar a un error
+// pero no hace crashear el programa.
+// Si la string comienza con un numero y luego tiene letras o simbolos, tomara
+// unicamente los primeros numeros que encuentre para convertirlos
+
+// console.log(parseInt(str6)) => NaN
+
+str6 = "25 de mayo de 1958";
+
+// console.log(parseInt(str6)) => 25
+
+//////////////////////////
+// 7- reordenar una string
+//////////////////////////
+// Hay casos donde necesitamos alterar una string para que cumpla con un
+// formato concreto, un ejemplo son las fechas estadounidenses, que se escriben
+// generalmente como: año-dia-mes
+// mientras que nosotros la escribimos: dia-mes-año
+
+var str7 = "2019-22-5";
+// supongamos que queremos reordenar la fecha de arriba a nuestro formato,
+//para esto podemos usar una mezcla de los pasos anteriores
+// primero la separamos en 3 partes, dia, mes y año usando split, para separarlos
+// vamos a usar el guión, que es la separación que se usa en la string
+
+var str7cortada = str7.split("-");
+// console.log(str7cortada) => [ '2019', '22', '5' ]
+
+// en este momento tenemos un array con los elementos de la fecha separados
+// pero siguen en el mismo orden, en este caso los lugares son:
+// año: lugar 0
+// dia: lugar 1
+// mes: lugar 2
+// y queremos reordenarlos para que queden dia mes año, es decir: 1,2,0
+// como es un array simplemente usamos los trozos y los unimos en una cadena
+
+var reordenada = str7cortada[1] + str7cortada[2] + str7cortada[0];
+
+// console.log(reordenada) => 2252019
+
+// en este caso los numeros aparecen todos juntos porque no pusimos ninguna
+// separación entre un elemento y otro, para esto, entre medio de un trozo y
+// otro pondremos un guión (-) o barra (/)
+
+reordenada = str7cortada[1] + "/" + str7cortada[2] + "/" + str7cortada[0];
+
+// console.log(reordenada) => 22/5/2019
+
+// es importante poner las barras entre comillas, porque van a ser una string
+// y siempre poner los signos de + cuando luego de lo que estamos escribiendo
+// va a venir otro trozo de texto o variable.
+
+/////////////////////////////////
+// 8- convertir texto a minusculas
+/////////////////////////////////
+// en algunos, vamos a tener que comparar 2 strings que esten
+// una con mayusculas y otra totalmente en minusculas, o una mezcla de ambas
+
+var str8min = "texto";
+var str8may = "TEXTO";
+
+// al compararlas, se considera que son distintas
+
+// console.log(str8min == str8may) => false
+
+// por lo que si se da el caso, suele ser util convertir ambas strings
+// a minusculas para asegurarse que el texto sea el mismo, sin importar como esten
+// escritas, la funcion que se encarga es toLowerCase(), es una funcion nativa
+// de las strings, y es importante escribir la L y la C de toLowerCase en mayusculas,
+// de otra forma no funciona, lo mismo con todos los nombres de funciones o variables
+// es fundamental escribir perfectamente los nombres respetando mayusculas
+
+// console.log(str8may.toLowerCase()) => texto
+
+// ahora puedo comparar
+
+// console.log(str8may.toLowerCase() == str8min.toLowerCase())  => true
+
+/////////////////////////////////////////////
+// 9 - mezclar texto con variables (avanzado)
+/////////////////////////////////////////////
+// hay una segunda manera mas nueva y mas comoda de mezclar texto
+// con variables en javascript, para esto es obligatorio
+// que la string vaya rodeada con este tipo de
+// comillas especiales ``: Lo que se hace es escribir las variables
+// entre llaves como la de los objetos, antecedidos por un signo de dolar
+
+var animal = "Conejo";
+
+var mezcla = `${animal} es un animal de las praderas del norte de mongolia`;
+
+// console.log(mezcla) => Conejo es un animal de las praderas del norte de mongolia
+
+// la gracia de esta forma, es que entre las llaves podes poner
+// expresiones simples, como condiciones o operaciones, y se ejecutan antes
+
+var cuenta = `2 x 2 = ${2 * 2}`;
+// console.log(cuenta) => 2 x 2 = 4
+
+// se pueden poner tantas variables como se quiera
+// tomando el ejemplo de las fechas
+
+// de la otra forma debiamos escribir
+reordenada = str7cortada[1] + "/" + str7cortada[2] + "/" + str7cortada[0];
+
+// con esta escribimos
+reordenada = `${str7cortada[1]}/${str7cortada[2]}/${str7cortada[0]}`;
+
+// y el resultado es el mismo pero mas corto de escribir
+
+// se puede escribir cualquier tipo de variable o funcion que retorne un valor
+// entre las llaves, por ejemplo
+
+var aleatorio = `Han pasado ${Math.random()} Años luz desde el domingo pasado`;
+// console.log(aleatorio) => Han pasado 0.0001491299407106 Años luz desde el domingo pasado
+
+/////////////////////////////////////
+// ejercicio de formateo de texto
+////////////////////////////////////
+// en japones los nombres se suelen decir
+// Primero el apellido y luego el nombre
+
+var nombre = "Uzumaki Naruto";
+
+// usando la funcion split() y la mezcla avanzada del punto 9, reordenar el
+// nombre para que diga Naruto Uzumaki.
