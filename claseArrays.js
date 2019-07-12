8bcbc29e3db5c65010aeacc4507a97f41432df45
fabran99 <46858770+fabran99@users.noreply.github.com>
2019-07-11 22:06:57 -0300

    Add files via upload

---
+++ claseArrays.js
@@ -0,0 +1,171 @@
+/////////////////////////////
+//arrays
+////////////////////////////
+// los arrays son listas formadas por otras variables, para definirlos simplemente
+// tenemos que rodear una serie de 0 o mas variables entre parentesis rectos
+
+//////////
+// 1- array simple
+//////////
+
+var array1 = [1, 2, 3, 4];
+
+// en este caso array1 contiene los numeros del 1 al 4
+// para acceder a sus valores, se debe seleccionar la posicion del valor dentro
+// de la lista, poniendo el numero del lugar donde se encuentra, contando desde 0
+
+// console.log(array1[0]) => 1
+// console.log(array1[1]) => 2
+// console.log(array1[3]) => 4
+
+// si se intenta elegir un lugar donde no existe un elemento, devolvera undefined
+
+// console.log(array1[1413]) => undefined
+
+///////////////////
+// 2- modificar valores de un array
+///////////////////
+
+var array2 = [1, 2, 3, 4, 5];
+
+// console.log(array2[0]) => 1
+
+// para modificar el valor de un elemento de un array, hay que igualarlo a el valor
+// nuevo, como si fuera cualquier otra variable
+
+array2[0] = 2019;
+// console.log(array2[0]) => 2019
+
+//////////////////////
+//3- agregar elementos a un array
+/////////////////////
+var array3 = [1, 2, 3, 4];
+
+// para agregar un elemento al final del array hay 2 formas, usando la funcion
+// push(valornuevo), que lo que hace es agregar un valor mas al array
+
+array3.push(5);
+// ahora el array pasa a tener un quinto valor
+// console.log(array3) => [ 1, 2, 3, 4, 5 ]
+
+// tambien se puede asignar un valor nuevo al array usando el numero del lugar
+// a donde va a quedar el valor nuevo, en este caso, en la posicion 5
+
+array3[5] = 6;
+// console.log(array3) => [ 1, 2, 3, 4, 5, 6 ]
+
+// lo mejor suele ser usar la pufuncion push(), porque de esta forma te aseguras
+// no dejar un espacio hueco, si usamos el segundo metodo y nos salteamos un lugar,
+// dejaremos un espacio vacio en el array, que luego puede dar problemas.
+
+array3[7] = 12;
+// console.log(array3) => [ 1, 2, 3, 4, 5, 6, <1 empty item>, 12 ]
+
+/////////////////////
+// 4- mezclar arrays
+////////////////////
+
+// en caso de querer mezclar 2 arrays, usamos la funcion concat().
+// se escribe array1.concat(array2, array3, array4....)
+
+var arrayInicial = [1, 2, 3, 4];
+var arraySecundario = [5, 6, 7, 8];
+
+// var mezcla = arrayInicial.concat(arraySecundario)
+// console.log(mezcla) => [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+
+////////////////////////
+// 5- buscar dentro de un array
+///////////////////////
+
+// si queremos buscar en que lugar esta un elemento de un array, usamos indexOf()
+// indexOf(parametro) => donde parametro es el valor que estamos buscando.
+// imaginemos que tenemos un ranking de elo, donde el que esta en primera posicion
+// es el mejor, y el que esta en ultima posicion es el peor.
+
+var ranking = ["Carrica", "Bardini", "Bunny", "Odazio", "Panes"];
+
+// si quiero saber en que posición del ranking estoy, uso indexOf(minombre)
+
+// console.log(ranking.indexOf("Panes")) => 4
+
+// si recordamos, en realidad los arrays empiezan desde el 0, por lo que lo logico
+// seria sumarle 1 al resultado
+
+// console.log(
+//   `Panes esta en el puesto ${ranking.indexOf("Panes") + 1} del ranking` =>
+// );
+// Panes esta en el puesto 5 del ranking
+
+///////////////////////////
+// 6 - Ordenar elementos de un array
+//////////////////////////
+// si queremos ordenar alfabeticamente los valores de un array
+// usamos la funcion array.sort()
+
+var nombres = ["Carrica", "Bardini", "Bunny", "Odazio", "Panes"];
+
+// console.log(nombres.sort()) => [ 'Bardini', 'Bunny', 'Carrica', 'Odazio', 'Panes' ]
+
+var numeros = [12, 1, 1.5, 5];
+// en el caso de los numeros, por defecto los va a ordenar alfabeticamente,
+// por lo que  el resultado que se espera no es el correcto, porque ignora
+// que son numeros
+
+// console.log(numeros.sort()) => [ 1, 1.5, 12, 5 ]
+
+// para este caso hay que usar una función hecha a mano, que se explicara
+// en la siguiente clase (quiza)
+
+//////////////////////////
+// 7- unir elementos de un array en una string
+///////////////////////////
+// para unir elementos usamos la funcion join()
+// join lleva como parametro el caracter que vamos a usar para
+// unir los elementos
+
+var elementos = ["pera", "manzana", "naranja"];
+
+var juntos = elementos.join(",");
+// console.log(juntos) => pera,manzana,naranja
+
+//////////////////
+// 8 - Arrays de varios niveles
+/////////////////
+
+// Los arrays pueden tener cualquier tipo de variable dentro, inclusive otros arrays
+var arrayMultinivel = [0, 5, [1, 4, 6], 6, 8];
+
+//en este caso el elemento numero 2 del array, es un array tambien
+
+// console.log(arrayMultinivel[2]) => [ 1, 4, 6 ]
+
+// si quiero seleccionar un elemento de ese array, simplemente agrego la posicion del elemento en ese array
+
+// console.log(arrayMultinivel[2][0]) => 1
+
+// y así sucesivamente tantas veces como arrays tenga, pudiendo terminar algo como arrayMultinivel[2][0][8][12] etc...
+
+///////////////////////////
+// 9 - Objetos dentro de un array
+//////////////////////////
+// se puede tener objetos dentro de un array
+
+var personas = [
+  {
+    nombre: "Bunny",
+    ocupacion: "Eloboosteado",
+    elo: "oro"
+  },
+  {
+    nombre: "Panes",
+    ocupacion: "Panadero",
+    elo: "bronce 3"
+  }
+];
+
+// el elemento 0 del array personas es bunny, y el 1 es panes, para acceder a sus propiedades,
+// hace falta agregar .nombredelapropiedad al final de personas[posiciondelobjeto]
+
+// console.log(personas[0].ocupacion) => Eloboosteado
+// primero entro a personas, luego a la posicion 0 del array que es el objeto bunny, y entonces busco su ocupación
---
+++ claseFunciones.js
@@ -0,0 +1,208 @@
+//////////////////////////////////
+// funciones
+/////////////////////////////////
+
+// las funciones son una operación que ejecuta javascript para realizar una tarea, las hay por defecto, y tambien
+// se pueden crear funciones propias para automatizar proceso que normalmente hacerlos a mano tardaria mucho
+
+//////////////////
+// 1- Funciones del sistema
+/////////////////
+
+// Las funciones se suelen ejecutar, o llamar, escribiendo su nombre y usando parentesis, que pueden o no
+// llevar algo dentro, los parametros son opcionales y dependen de la función que estas ejecutando
+
+Math.random();
+// es una función del objeto Math, que es parte del sistema, esta función genera un numero
+// al azar entre 0 y 1, y no lleva ningun parámetro, porque no varía, siempre hace lo mismo
+
+// console.log() es una función del objeto consola, lo que pongas como parámetro entre paréntesis,
+// aparece en la consola, ya sean variables, texto, etc.
+
+// hay funciones que son exclusivamente para un tipo de variable, por ejemplo las de las strings
+
+var str = "MAYUSCULAS";
+
+// console.log(str.toLowerCase()) => mayusculas
+// lo que hace es cambiar toda la string a minusculas
+
+//////////////////////////
+// 2- Funciones personalizadas
+/////////////////////////
+
+// para crear una función personalizada, hay 2 maneras, una simple y otra avanzada que por el momento
+// no voy a explicar, para hacerla, escribimos function nombrequelepongoalafuncion y parentesis, luego llaves
+
+function sumar() {}
+
+// lo que esta entre paréntesis son los parámetros que va a utilizar, son opcionales, por lo que puede ir vacío
+// lo que esta entre las llaves va a ser todas las tareas que va a ejecutar la función
+// en este caso vamos a crear una función que sume 2 numeros y los muestre en la consola
+
+var numero1 = 18;
+var numero2 = 15;
+
+function sumar() {
+  console.log(numero1 + numero2);
+}
+
+// si la dejamos así, la funcion no hace nada, simplemente existe como si fuera una variable, tenemos que ejecutarla
+// como las demas, escribiendo su nombre y parentesis
+
+// sumar() => esto hara que en la consola aparezca 33
+
+// Sin embargo ahora tenemos que cambiarle manualmente el valor a las variables
+// para volver a sumar y que de algo diferente, asique si queremos sumar otros 2 numeros, tenemos que
+// cambiarles el valor a las variables que sumamos dentro de la función, y recien ahí volverla a ejecutar
+// para que sume los numeros nuevos
+
+numero1 = 2;
+numero2 = 15;
+
+//sumar() => 17
+
+// Por el momento, la función no equivale a un número como en el caso de Math.random(), que podemos multiplicarla
+// asumiendo que es un numero al azar, si hacemos sumar() * 5, simplemente va a mostrar 17 en la consola, esto es porque
+// no hicimos que la función retornara algo, es decir, la función no equivale a la suma, sino que lo que hace es mostrar
+// la suma en la consola.
+// para que podamos usarla en cuentas, tenemos que usar el método return
+
+///////////////////
+// 3 - funciones que retornan valores
+////////////////////
+
+function sumar() {
+  return numero1 + numero2;
+}
+
+// console.log(sumar() * 5) => 85
+
+// para hacer esto, escribimos return, seguido de la operación que queremos que equivalga, en este caso numero1 + numero2
+// de esta forma, le decimos, sumar() equivale a numero1 + numero2, es exactamente igual que si usaramos una variable simple
+
+var suma = numero1 + numero2;
+
+// console.log(suma * 5) => 85
+
+// la diferencia, es que mientras las variables tienen limitantes a la hora de declararlas, las funciones pueden procesar
+// los datos de tantas formas como uno quiera, todo lo que esté entre las llaves {} se va a ejecutar antes de retornar
+// un valor
+
+var total = 255;
+var porcentaje = 20;
+
+// esta función retorna el 20% del total pero como una frase entera que contiene el resultado.
+
+function calcularPorcentaje() {
+  var resultado = (20 * 255) / 100;
+
+  return `El ${porcentaje}% de ${total} es ${resultado}`;
+}
+
+// console.log(calcularPorcentaje()) => El 20% de 255 es 51
+
+// si bien retornar valores es útil, no implica que todas las funciónes deban retornar algo,
+// muchas veces solo necesitamos que haga algo concreto, como modificar una variable,
+// o mostrar algo en consola si se da una condición
+
+var edad = 18;
+
+function revisar() {
+  if (edad >= 18) {
+    console.log("esta permitido");
+  } else {
+    console.log("no esta permitido");
+  }
+}
+
+// revisar() => muestra en la consola "esta permitido";
+
+//////////////////////////////
+// 4 - funciones con parámetros
+///////////////////////////
+
+// sin embargo, la mayor utilidad de las funciónes es reciclar código, para poder utilizarlo varias veces
+// a lo largo del programa ahorrando repetir tareas que se hagan varias veces,
+// la mejor forma de hacerlo, es usando parametros, de esta forma no dependemos
+// de una variable concreta para hacerla funcionar, sino que usando la misma función siempre, variando su resultado
+// segun el parámetro que le pongamos entre parentesis, de la misma forma que console.log() se puede usar decenas de veces
+// en cada programa, cambiando el contenido que va entre los paréntesis obtenes distintos resultados
+
+// console.log("resultado 1") => resultado 1
+// console.log("resultado 2") => resultado 2
+// console.log("resultado 3") => resultado 3
+
+// resultado distinto dependiendo del parámetro, pero la función sigue siendo console.log()
+
+function sumar(primero, segundo) {
+  return primero + segundo;
+}
+
+// para agregarle parametros a una función, al declararla, debemos ponerle un nombre a los parámetros que se vayan
+// a usar, de esta forma, usamos ese mismo nombre de variable dentro de la función
+
+// para llamar la funcion ahora, no basta con poner suma(), sino que entre paréntesis hay que poner los 2 valores que
+// se van a sumar, separados por coma
+
+// sumar(5, 10 ) => equivale a 15, ahora como equivale a un numero, puedo usarlo para hacer mas operaciones
+
+// si quiero calcular el cuadrado de la suma de 2 numeros, los sumo y los multiplico
+// console.log(sumar(5, 10) * sumar(5, 10)) => 225
+
+// Por supuesto, los parametros pueden ser cualquier tipo de variable, eso va a depender del tipo de función que estemos
+// haciendo, en este caso vamos a hacer una función que retorne un numero entero al azar, que se encuentre entre 2 numeros,
+// el primer parámetro sera el numero minimo, y el segundo parametro será el maximo
+// de forma que si llamamos azar(0, 5) => deberia retornar un numero entre 0 y 5 incluido
+
+function azar(minimo, maximo) {
+  // como se explicó en la claseNumeros, para hacer un numero al azar entre 2 numeros, hay que multiplicar
+  // Math.random() por el tome máximo, y sumarle el mínimo
+  var resultado = minimo + Math.random() * maximo;
+
+  // el numero que tenemos hasta ahora, efectivamente esta entre 0 y 5, pero hay que redondearlo para que
+  // de un entero como queremos, para esto retornamos el resultado con Math.round(resultado)
+
+  return Math.round(resultado);
+}
+
+// console.log(azar(0, 5)) => cada vez que ejecutemos la función dará un numero entre 0 y 5
+// ahora esta función la podemos reutilizar tantas veces como necesitemos, cambiando los parámetros que necesitemos
+// por ejemplo, en el caso de que queramos simular una tirada de moneda, ponemos un numero entre 0 y 1, y dependiendo
+// de lo que salga, decimos que es cara o cruz
+
+function tirarMoneda() {
+  if (azar(0, 1) == 0) {
+    console.log("Cara");
+  } else {
+    console.log("Cruz");
+  }
+}
+
+// tirarMoneda() => muestra en la consola  cara o cruz aleatoriamente, también podemos hacer que retorne el valor
+
+function tirarMoneda() {
+  if (azar(0, 1) == 0) {
+    return "Cara";
+  } else {
+    return "Cruz";
+  }
+}
+
+// console.log(tirarMoneda()) => Cara o Cruz aleatoriamente
+
+////////////////////////
+// ejercicio: crear una tirada de dados
+////////////////////////
+
+// dentro de una función se puede poner tanto código como sea necesario, y ese codigo se ejecutará cada vez que llamemos
+// a la función, en este caso el ejercicio consiste en hacer una función que retorne una tirada de dados al azar,
+// es decir que elija al azar un número entre 1 y 6, y compare lo que salió con lo que pongas como parámetro
+
+// ejemplo: tirarDados(5) => si el numero al azar es 5 (el parámetro que elegí),
+// deberia poner en la consola "acertaste", de lo contrario "fallaste"
+// ejemplo: tirarDados(4) => si el numero al azar es 5 (el parámetro que elegí),
+// deberia poner en la consola "acertaste", de lo contrario "fallaste"
+
+// en caso de que la persona ponga un número que sea menor a 1, o mayor a 6 hay que poner una alerta
+// y en vez de retornar "acertaste" o "fallaste" debería retornar "Numero incorrecto", ya que los dados tienen solo
+// 6 caras
